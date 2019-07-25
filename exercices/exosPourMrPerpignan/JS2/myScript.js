246a52c4548d76e34036abfedfac3a44f1ef3744
Maxadlib <lemaildiamant@gmail.com>
2019-07-25 14:05:30 +0200

    JS2 - exercice 2 en cours... 8-$

--- exercices/exosPourMrPerpignan/JS2/myScript.js
+++ exercices/exosPourMrPerpignan/JS2/myScript.js
@@ -1,7 +1,6 @@
 
 // JS2 : Exercice 1
 var randomNumber = Math.round(Math.random()*100);
-console.log(randomNumber);
 
 var newParagraph = document.createElement("p");
 
@@ -10,5 +9,26 @@ var random100 = document.createTextNode("Voici un nombre aléatoire entre 10 et 
 newParagraph.appendChild(random100);
 
 document.getElementById("JS2_1").appendChild(newParagraph);
+
+// FIN Exercice 1
+
+// JS2 : Exercice 2
+var aNumber = Math.round(Math.random()*10);
+var bNumber = Math.round(Math.random()*10);
+
+function produit(){
+  var btnProduit = document.getElementById("btnProduit");
+  return aNumber * bNumber;
+  btnProduit.onclick = alert("Bonjour !");
+;}
+
+var newParagraph2 = document.createElement("p");
+newParagraph2.appendChild(random100);
+document.getElementById("JS2_2").appendChild(newParagraph2);
+
+btnProduit.onclick= produit;
+
+window.onload="initElement();";
+// console.log(produit);
 
 // FIN Exercice 1
